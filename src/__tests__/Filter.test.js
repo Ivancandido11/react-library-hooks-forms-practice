e11abe796761d4884d695c0271468d6ff196374e
Gillian Wenhold <gillian.wenhold@powerhrg.com>
2021-07-07 10:11:23 -0400

    Finish filter tests

--- src/__tests__/Filter.test.js
+++ src/__tests__/Filter.test.js
@@ -1,28 +1,21 @@
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import Filter from "../components/Filter";
-import ShoppingList from "../components/ShoppingList";
+import BookList from "../components/BookList";
 
 const testData = [
-  { id: 1, name: "Yogurt", category: "Dairy" },
-  { id: 2, name: "Pomegranate", category: "Produce" },
-  { id: 3, name: "Lettuce", category: "Produce" },
-  { id: 4, name: "String Cheese", category: "Dairy" },
-  { id: 5, name: "Swiss Cheese", category: "Dairy" },
-  { id: 6, name: "Cookies", category: "Dessert" },
+  { id: 1, title: "A People's History", author: "Howard Zinn", genre: "History", image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1494279423l/2767._SY475_.jpg" },
+  { id: 2, title: "Belonging", author: "Toko-pa Turner", genre: "Nonfiction", image: "https://images-na.ssl-images-amazon.com/images/I/713kd5KWXIL.jpg" },
+  { id: 3, title: "The Gifts of Imperfection", author: "Brene Brown", genre: "Nonfiction", image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1369092544l/7015403.jpg" },
+  { id: 4, title: "An Indigenous Peoples' History", author: "Roxanne Dunbar-Ortiz", genre: "History", image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1395003842l/20588662.jpg" },
+  { id: 5, title: "The One Thing", author: "Gary Keller and Jay Papasan", genre: "Business", image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1559563270l/16256798._SX318_.jpg" },
 ];
 
 // Filter
 const noop = () => {};
-test("uses a prop of 'search' to display the search term in the input field", () => {
-  render(<Filter search="testing" onSearchChange={noop} />);
-
-  expect(screen.queryByPlaceholderText(/Search/).value).toBe("testing");
-});
-
-test("calls the onSearchChange callback prop when the input is changed", () => {
+test("calls the onTextMatchChange callback prop when the input is changed", () => {
   const onChange = jest.fn();
-  render(<Filter search="testing" onSearchChange={onChange} />);
+  render(<Filter onGenreChange={noop} onTextMatchChange={onChange} />);
 
   fireEvent.change(screen.queryByPlaceholderText(/Search/), {
     target: { value: "testing123" },
@@ -32,7 +25,7 @@ test("calls the onSearchChange callback prop when the input is changed", () => {
 });
 
 test("the input field acts as a controlled input", () => {
-  render(<ShoppingList items={testData} />);
+  render(<BookList books={testData} />);
 
   fireEvent.change(screen.queryByPlaceholderText(/Search/), {
     target: { value: "testing 123" },
@@ -41,41 +34,34 @@ test("the input field acts as a controlled input", () => {
   expect(screen.queryByPlaceholderText(/Search/).value).toBe("testing 123");
 });
 
-// Shopping List
-test("the shopping list displays all items when initially rendered", () => {
-  const { container } = render(<ShoppingList items={testData} />);
+// Book list
+test("the book list displays all items when initially rendered", () => {
+  const { container } = render(<BookList books={testData} />);
   expect(container.querySelector(".Items").children).toHaveLength(
     testData.length
   );
 });
 
-test("the shopping filters based on the search term to include full matches", () => {
-  render(<ShoppingList items={testData} />);
+test("the book list filters based on the search term to include full matches", () => {
+  render(<BookList books={testData} />);
 
   fireEvent.change(screen.queryByPlaceholderText(/Search/), {
-    target: { value: "Yogurt" },
+    target: { value: "Toko-pa Turner" },
   });
 
-  expect(screen.queryByText("Yogurt")).toBeInTheDocument();
-  expect(screen.queryByText("Lettuce")).not.toBeInTheDocument();
-
-  fireEvent.change(screen.queryByPlaceholderText(/Search/), {
-    target: { value: "Lettuce" },
-  });
-
-  expect(screen.queryByText("Lettuce")).toBeInTheDocument();
-  expect(screen.queryByText("Yogurt")).not.toBeInTheDocument();
+  expect(screen.queryByText("Belonging")).toBeInTheDocument();
+  expect(screen.queryByText("The Gifts of Imperfection")).not.toBeInTheDocument();
 });
 
-test("the shopping filters based on the search term to include partial matches", () => {
-  render(<ShoppingList items={testData} />);
+test("the book list filters based on the search term to include partial matches", () => {
+  render(<BookList books={testData} />);
 
   fireEvent.change(screen.queryByPlaceholderText(/Search/), {
-    target: { value: "Cheese" },
+    target: { value: "History" },
   });
 
-  expect(screen.queryByText("Swiss Cheese")).toBeInTheDocument();
-  expect(screen.queryByText("String Cheese")).toBeInTheDocument();
-  expect(screen.queryByText("Lettuce")).not.toBeInTheDocument();
-  expect(screen.queryByText("Yogurt")).not.toBeInTheDocument();
+  expect(screen.queryByText("A People's History")).toBeInTheDocument();
+  expect(screen.queryByText("An Indigenous Peoples' History")).toBeInTheDocument();
+  expect(screen.queryByText("Belonging")).not.toBeInTheDocument();
+  expect(screen.queryByText("The One Thing")).not.toBeInTheDocument();
 });
